0b0c1304aa84af8e232f5d066e51aeb7528720de
Sami Ukkonen <sami.ukkonen@gmail.com>
2022-04-29 06:26:44 +0300

    Start shop_demo project

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import express, {Express, Request, Response} from 'express';
+import dotenv from 'dotenv';
+
+
+dotenv.config();
+
+const app: Express = express();
+const port = process.env.PORT;
+
+
+app.get('/', (reg: Request, res: Response) => {
+    res.send("Express + Typescript Server");
+})
+
+app.listen(port, () => {
+    console.log(`⚡️[server]: Server is running at https://localhost:${port}`);
+})
+
+
